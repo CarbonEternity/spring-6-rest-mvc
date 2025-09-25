ae229f77d061a557632d632954a1789fdcd437f6
CarbonEternity <pa03622789@gmail.com>
2025-09-25 18:44:59 +0200

    Rest controller assessment

---
+++ src/main/java/com/springframework/spring6restmvc/services/CustomerService.java
@@ -0,0 +1,13 @@
+package com.springframework.spring6restmvc.services;
+
+import com.springframework.spring6restmvc.model.Customer;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface CustomerService {
+
+    public List<Customer> getAllCustomers();
+
+    public Customer getCustomerById(UUID id);
+}

d4fef4f7806d494fa7e487617b3e6e5d9cb2a74b
CarbonEternity <pa03622789@gmail.com>
2025-10-08 22:36:04 +0200

    Use ServiceConnection instead of DynamicPropertySource

--- src/test/java/com/springframework/spring6restmvc/repositories/MySqlTest.java
+++ src/test/java/com/springframework/spring6restmvc/repositories/MySqlTest.java
@@ -5,14 +5,12 @@ import com.springframework.spring6restmvc.entities.Beer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-import javax.sql.DataSource;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -24,18 +22,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class MySqlTest {
 
     @Container
+    @ServiceConnection
     static MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:8.0");
 
-    @DynamicPropertySource
     // for not to use the values from localmysql profile properties file
-    static void setDatasourceProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
-        registry.add("spring.datasource.username", mySQLContainer::getUsername);
-        registry.add("spring.datasource.password", mySQLContainer::getPassword);
-    }
-
-    @Autowired
-    DataSource dataSource;
+//    @DynamicPropertySource
+//    static void setDatasourceProperties(DynamicPropertyRegistry registry) {
+//        registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
+//        registry.add("spring.datasource.username", mySQLContainer::getUsername);
+//        registry.add("spring.datasource.password", mySQLContainer::getPassword);
+//    }
 
     @Autowired
     BeerRepository beerRepository;

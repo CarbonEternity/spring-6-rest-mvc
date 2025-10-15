d685f395468ea16e6cc32318d77dfee106029465
CarbonEternity <pa03622789@gmail.com>
2025-10-15 17:08:28 +0200

    Load CSV data to database in Bootstrap process

--- src/test/java/com/springframework/spring6restmvc/bootstrap/BootstrapDataTest.java
+++ src/test/java/com/springframework/spring6restmvc/bootstrap/BootstrapDataTest.java
@@ -2,6 +2,7 @@ package com.springframework.spring6restmvc.bootstrap;
 
 import com.springframework.spring6restmvc.repositories.BeerRepository;
 import com.springframework.spring6restmvc.repositories.CustomerRepository;
+import com.springframework.spring6restmvc.services.BeerCsvService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,15 +19,18 @@ class BootstrapDataTest {
     @Autowired
     CustomerRepository customerRepository;
 
+    @Autowired
+    BeerCsvService csvService;
+
     BootstrapData bootstrapData;
 
     @BeforeEach
     void setUp() {
-        bootstrapData = new BootstrapData(beerRepository, customerRepository);
+        bootstrapData = new BootstrapData(beerRepository, customerRepository, csvService);
     }
 
     @Test
-    void run() {
+    void run() throws Exception {
         bootstrapData.run(null);
 
         assertThat(beerRepository.count()).isEqualTo(3);

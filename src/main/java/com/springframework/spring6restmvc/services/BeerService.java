ed9022f2b7e8c2238ff310654c8ca996014660ac
CarbonEternity <pa03622789@gmail.com>
2025-10-16 19:24:36 +0200

    Add paging and sorting

--- src/main/java/com/springframework/spring6restmvc/services/BeerService.java
+++ src/main/java/com/springframework/spring6restmvc/services/BeerService.java
@@ -2,14 +2,15 @@ package com.springframework.spring6restmvc.services;
 
 import com.springframework.spring6restmvc.model.BeerDTO;
 import com.springframework.spring6restmvc.model.BeerStyle;
+import org.springframework.data.domain.Page;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface BeerService {
 
-    List<BeerDTO> getAllBears(String beerName, BeerStyle beerStyle, Boolean showInventory);
+    Page<BeerDTO> getAllBears(String beerName, BeerStyle beerStyle, Boolean showInventory,
+                              Integer pageNumber, Integer pageSize);
 
     Optional<BeerDTO> getBeerById(UUID id);
 

34fd47cdb7457dce8c7ae2bbbf8ca180f3063f1a
CarbonEternity <pa03622789@gmail.com>
2025-10-17 19:54:49 +0200

    Add a category table in migration, add entity a relationship to beer

---
+++ src/test/java/com/springframework/spring6restmvc/repositories/CategoryRepositoryTest.java
@@ -0,0 +1,39 @@
+package com.springframework.spring6restmvc.repositories;
+
+import com.springframework.spring6restmvc.entities.Beer;
+import com.springframework.spring6restmvc.entities.Category;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+class CategoryRepositoryTest {
+
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    Beer testBeer;
+
+    @BeforeEach
+    void setUp() {
+        testBeer = beerRepository.findAll().get(0);
+    }
+
+    @Transactional
+    @Test
+    void testAddCategory() {
+        Category category = categoryRepository.save(Category.builder()
+                .description("Ales")
+                .build());
+
+        testBeer.addCategory(category);
+        Beer savedBeer = beerRepository.save(testBeer);
+
+        System.out.println(savedBeer.getBeerName());
+    }
+}
